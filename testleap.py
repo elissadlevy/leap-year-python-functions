9157b382b9b27d7ec5be600e2308da276fab32a4
Aaron <afink.2020@saintannsny.org>
2018-12-16 21:33:09 -0500

    Update testleap.py

--- testleap.py
+++ testleap.py
@@ -1,7 +1,7 @@
 import leap
 
-# Test out your methods by calling them here. Remeber that because they are in a different file, you must reference them with a dot like "leap.is_leap_year(1997)".
-# We've written two tests for your first method, so these two should work once you successfully code out the is_leap_year method.
+# Test out your functions by calling them here. Remeber that because they are in a different file, you must reference them with a dot like "leap.is_leap_year(1997)".
+# We've written two tests for your first function, so these two should work once you successfully code out the is_leap_year function.
 
 print(leap.is_leap_year(1997))
 
