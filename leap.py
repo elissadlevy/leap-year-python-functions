3cf9104d5df1d25333ddc1963b00f2f1ccb3c829
Aaron <afink.2020@saintannsny.org>
2018-12-27 12:19:29 -0500

    change pass to return

--- leap.py
+++ leap.py
@@ -2,7 +2,7 @@
 
 # 1. Write a function that checks to see if a given year is a leap year, by replacing "pass" with your own code, and returns either the boolean value True or False.
 def is_leap_year(year):
-   pass
+   return
 
 
 # 2. Write a function that takes in the current year and returns a string telling when the next leap year will be.
